b9fb81b31e1bd75ee4735716351fd0b438bc6a02
Badara <bbabadara@gmail.com>
2026-04-23 03:01:19 +0000

    ajout du record sku

--- src/main/java/org/produit/valueobjects/SKU.java
+++ src/main/java/org/produit/valueobjects/SKU.java
@@ -1,4 +1,12 @@
 package org.produit.valueobjects;
 
+import org.produit.exception.InvalidSKUException;
+
 public record SKU(String value) {
+    private static final String REGEX = "^[A-Z]{3}-\\d{4,6}$";
+    public SKU {
+        if (!value.matches(REGEX) || value == null) {
+            throw new InvalidSKUException("Le format du SKU est invalide (Format attendu : AAA-1234 à AAA-123456) : " + value);
+        }
+    }
 }
